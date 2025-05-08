119ae885872a532fe921d01faca43bfd380f020d
AFungo <augustofungo@gmail.com>
2025-05-08 10:45:44 -0300

    Add repository for user database

---
+++ user-service/src/main/java/com/coworking/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.coworking.user.repository;
+
+import com.coworking.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByName(String name);
+
+}
