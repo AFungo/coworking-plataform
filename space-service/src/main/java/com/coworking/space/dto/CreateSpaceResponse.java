ddaf2419b75cdf959abf3147d86daa36d9b4e3f1
AFungo <augustofungo@gmail.com>
2025-06-02 12:52:11 -0300

    Create createSpace service

---
+++ space-service/src/main/java/com/coworking/space/dto/CreateSpaceResponse.java
@@ -0,0 +1,10 @@
+package com.coworking.space.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CreateSpaceResponse {
+    private long id;
+}
