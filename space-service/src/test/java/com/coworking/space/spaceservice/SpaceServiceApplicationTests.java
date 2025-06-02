8e62ed634a784a0be8f2df8c4930be318e55c2d8
AFungo <augustofungo@gmail.com>
2025-06-02 12:10:25 -0300

    Init space-service

---
+++ space-service/src/test/java/com/coworking/space/spaceservice/SpaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coworking.space.spaceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
